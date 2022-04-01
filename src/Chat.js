bbb69575cc02116740c4b97fcf344ec506c14067
Anirudh R <anirudh0288@gmail.com>
2022-04-02 02:21:59 +0530

    Project Visualization

---
+++ src/Chat.js
@@ -0,0 +1,70 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import {
+  AttachFile,
+  InsertEmoticon,
+  Mic,
+  MoreVert,
+  SearchOutlined,
+} from "@material-ui/icons";
+import React, { useEffect, useState } from "react";
+import "./Chat.css";
+
+function Chat() {
+  const [input, setInput] = useState("");
+  const [seed, setSeed] = useState("");
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    console.log("You typed =>", input);
+  };
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>Last Seen at ...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p className={`chat__message ${true && "chat__reciever"}`}>
+          <span className="chat__name">Liebermann</span>Example
+          <span className="chat__timestamp">10:10 AM</span>
+        </p>
+        <p className="chat__message">sup</p>
+      </div>
+      <div className="chat__footer">
+        <InsertEmoticon />
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Type a message"
+            type="text"
+          />
+          <button onClick={sendMessage} type="submit">
+            Send
+          </button>
+        </form>
+        <Mic />
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAtUZTx-vZAguqwqVKzG1Gnzo8gfeNTNKs",
+    authDomain: "whatsapp-clone-90f88.firebaseapp.com",
+    projectId: "whatsapp-clone-90f88",
+    storageBucket: "whatsapp-clone-90f88.appspot.com",
+    messagingSenderId: "176651484775",
+    appId: "1:176651484775:web:c5756d463cc70ad2959ec2",
+    measurementId: "G-STNJX5DJXP"
+  };
